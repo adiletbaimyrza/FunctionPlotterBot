dc9a484b76990837e87d2d0fe99b42bc6726b87a
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-03-02 22:49:05 +0100

    last upgrade

--- myTurtle.py
+++ myTurtle.py
@@ -1,12 +1,17 @@
 import plotter
 import math
 
-equation = plotter.EquationVisual(dimensions=100, tick=2, dot_size=3, precision=10, zoom=-5, mode='light') # default values: 100, 2, 3, 10, -5, 'light'
+equation = plotter.Quadratic()
 
-equation.open_screen(zoom=90) # default value: -5
-equation.set_mode('dark') # default value: 'light'
+equation.set_dimensions(20)
+equation.set_dot_size(3)
+equation.set_mode('dark')
+equation.set_precision(10)
+equation.set_tick(5)
+equation.set_zoom(10)
+
+equation.open_screen()
+
 equation.draw_plane()
 
-# equation.plot_equation(2, -5) # linear equation
-# equation.plot_equation(-1, 2, 1) # quadratic equation
-# equation.plot_trigonometric(2, 0, math.cos) # trigonometric equation
+equation.plot_equation(0.1, 1, 0)
--- plotter.py
+++ plotter.py
@@ -1,86 +1,72 @@
 import turtle
-from tkinter import *
 
+DIMENSIONS = 100
+TICK = 1
+DOT_SIZE = 3
+PRECISION = 10
+ZOOM = -5
+LIGHT = 'light'
+DARK = 'dark'
 DELAY = 0
+DEGREE90 = 90
+FONT = 'Arial'
+FONT_SIZE = 12
+FONT_TYPE = 'normal'
 
-class EquationVisual:
-    def __init__(self, dimensions=50, tick=1, dot_size=3, precision=5, zoom=-5, mode='light'):
-        self.dimensions = dimensions
-        self.tick = tick
-        self.dot_size = dot_size
-        self.zoom = zoom
-        self.mode = mode
-        
-        self.x_range = range(-dimensions *  precision, dimensions * precision)
+class Equation:
+    def __init__(self):
+        self.dimensions = DIMENSIONS
+        self.tick = TICK
+        self.dot_size = DOT_SIZE
+        self.precision = PRECISION
+        self.zoom = ZOOM
+        self.mode = LIGHT
+        self.font = FONT
+        self.font_size = FONT_SIZE
+        self.font_type = FONT_TYPE
+
+        dms = self.dimensions
+        pcs = self.precision
+        self.x_range = range(-dms *  pcs, dms * pcs)
+
         self.turtle = turtle.Turtle()
         self.screen = turtle.Screen()
-        self.precision = precision
         turtle.delay(DELAY)
 
-    def set_dimensions(self, dimensions):
+    def set_dimensions(self, dimensions=DIMENSIONS):
         self.dimensions = dimensions
 
-    def set_tick(self, tick):
+    def set_tick(self, tick=TICK):
         self.tick = tick
 
-    def set_dot_size(self, dot_size):
+    def set_dot_size(self, dot_size=DOT_SIZE):
         self.dot_size = dot_size
 
-    def set_mode(self, mode='light'):
+    def set_precision(self, precision=PRECISION):
+        self.precision = precision
+
+    def set_zoom(self, zoom=ZOOM):
+        self.zoom = zoom
+
+    def set_mode(self, mode=LIGHT):
         self.mode = mode
 
-    def open_screen(self, zoom=-5):
-        self.zoom = -zoom
-        self.screen.setworldcoordinates(-self.dimensions - self.zoom,
-                                            -self.dimensions - self.zoom,
-                                            self.dimensions + self.zoom,
-                                            self.dimensions + self.zoom)
+    def open_screen(self):
+        zoom = -self.zoom
+        dms = self.dimensions
+        self.screen.setworldcoordinates(-dms - zoom,
+                                        -dms - zoom,
+                                         dms + zoom,
+                                         dms + zoom)
 
-    def plot_equation(self, a, b, c=None):
-        self.turtle.pencolor('red')
-        self.turtle.showturtle()
-        self.turtle.penup()
-
-        if c == None:
-            for x in self.x_range:
-                x = float(x) / self.precision
-                y = a * x + b
-
-                self.turtle.goto(x, y)
-                self.turtle.pendown()
-        
-        if c != None:
-            for x in self.x_range:
-                x = float(x) / self.precision
-                y = a * pow(x, 2) + b * x + c
-
-                self.turtle.goto(x, y)
-                self.turtle.pendown()
-        
-        self.turtle.hideturtle()
-        self.screen.exitonclick()
-
-    def plot_trigonometric(self, a, b, type):
-        self.turtle.showturtle()
-        self.turtle.penup()
-        self.turtle.pencolor("red")
-
-        for x in self.x_range:
-            x = float(x) / self.precision
-            y = a * type(x) + b
-
-            self.turtle.goto(x, y)
-            self.turtle.pendown()
-        
-        self.turtle.hideturtle()
-        self.screen.exitonclick()
+    def plot_equation(self):
+        pass
 
     def draw_plane(self):
-
-        if self.mode == 'lihgt':
+        if self.mode == LIGHT:
             self.screen.bgcolor('white')
             self.turtle.pencolor('black')
-        elif self.mode == 'dark':
+        elif self.mode == DARK:
             self.screen.bgcolor('black')
             self.turtle.pencolor('green')
 
@@ -93,21 +79,113 @@ class EquationVisual:
             self.turtle.setposition(position)
 
             for _ in range(0, self.dimensions, self.tick):
+                temp = self.turtle.position()
+                if not turned:
+                    x = self.turtle.position()[0]
+                    y = self.turtle.position()[1]
+                    y += float(self.tick / 5)
+                    self.turtle.penup()
+                    self.turtle.setposition(x, y)
+                    self.turtle.write(_)
+                    self.turtle.setposition(temp)
+                    self.turtle.pendown()
+                if turned:
+                    x = self.turtle.position()[0]
+                    y = self.turtle.position()[1]
+                    x += float(self.tick / 5)
+                    self.turtle.penup()
+                    self.turtle.setposition(x, y)
+                    self.turtle.write(_)
+                    self.turtle.setposition(temp)
+                    self.turtle.pendown()
+
                 self.turtle.forward(self.tick)
                 self.turtle.dot(self.dot_size)
 
             self.turtle.setposition(position)
 
             for _ in range(0, self.dimensions, self.tick):
+                temp = self.turtle.position()
+                if not turned:
+                    x = self.turtle.position()[0]
+                    y = self.turtle.position()[1]
+                    y += float(self.tick / 5)
+                    self.turtle.penup()
+                    self.turtle.setposition(x, y)
+                    self.turtle.write(-_)
+                    self.turtle.setposition(temp)
+                    self.turtle.pendown()
+                if turned:
+                    x = self.turtle.position()[0]
+                    y = self.turtle.position()[1]
+                    x += float(self.tick / 5)
+                    self.turtle.penup()
+                    self.turtle.setposition(x, y)
+                    self.turtle.write(-_)
+                    self.turtle.setposition(temp)
+                    self.turtle.pendown()
                 self.turtle.backward(self.tick)
                 self.turtle.dot(self.dot_size)
 
             if not turned:
-                self.turtle.setheading(90)
+                self.turtle.setheading(DEGREE90)
                 turned = True
 
         self.turtle.hideturtle()
 
-    def download(self):
-        canvas = self.turtle.getscreen().getcanvas()
-        canvas.postscript(file='C:\\Users\\adile\\OneDrive\\Рабочий стол\\equationsVisualizer\\en.eps')
+class Linear(Equation):
+    def __init__(self):
+        super().__init__()
+    
+    def plot_equation(self, a, c):
+        self.turtle.pencolor('red')
+        self.turtle.showturtle()
+        self.turtle.penup()
+
+        for x in self.x_range:
+            x = float(x) / self.precision
+            y = a * x + c
+
+            self.turtle.goto(x, y)
+            self.turtle.pendown()
+        
+        self.turtle.hideturtle()
+        self.screen.exitonclick()
+
+class Quadratic(Equation):
+    def __init__(self):
+        super().__init__()
+
+    def plot_equation(self, a, b, c):
+        self.turtle.pencolor('red')
+        self.turtle.showturtle()
+        self.turtle.penup()
+
+        for x in self.x_range:
+            x = float(x) / self.precision
+            y = a * pow(x, 2) + b * x + c
+
+            self.turtle.goto(x, y)
+            self.turtle.pendown()
+        
+        self.turtle.hideturtle()
+        self.screen.exitonclick()
+
+class Trigonometric(Equation):
+    def __init__(self):
+        super().__init__()
+
+    def plot_equation(self, a, c, type):
+        self.turtle.pencolor('red')
+        self.turtle.showturtle()
+        self.turtle.penup()
+
+        for x in self.x_range:
+            x = float(x) / self.precision
+            y = a * type(x) + c
+
+            self.turtle.goto(x, y)
+            self.turtle.pendown()
+        
+        self.turtle.hideturtle()
+        self.screen.exitonclick()
