7550b3231434c8789f4abb3b6795d2052c6cc9a6
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-03-15 11:18:15 +0100

    last update

---
+++ bot.py
@@ -0,0 +1,5 @@
+import telegram.ext
+
+Token = '6220004213:AAGuemjvfG0-p7fNO7BPKTEZXftdtoGftMU'
+
+updater = telegram.ext._updater('6220004213:AAGuemjvfG0-p7fNO7BPKTEZXftdtoGftMU', use_context=True)
